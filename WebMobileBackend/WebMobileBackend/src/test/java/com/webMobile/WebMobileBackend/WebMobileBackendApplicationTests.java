a2ce2ccfec2daf6e4ecb2382690515749e5ce83d
Riffer10000 <35526854+Riffer10000@users.noreply.github.com>
2020-06-09 13:46:56 +0200

    Spring-Backend
    Dit is de spring backend webandmobile

---
+++ WebMobileBackend/WebMobileBackend/src/test/java/com/webMobile/WebMobileBackend/WebMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webMobile.WebMobileBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMobileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
