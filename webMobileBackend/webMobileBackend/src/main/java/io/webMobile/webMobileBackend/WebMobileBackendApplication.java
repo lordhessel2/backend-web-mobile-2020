67bc63ef930d11a01703ec5611b4b561f64874ac
lordhessel2 <36540564+lordhessel2@users.noreply.github.com>
2020-06-13 15:18:03 +0200

    Add files via upload

---
+++ webMobileBackend/webMobileBackend/src/main/java/io/webMobile/webMobileBackend/WebMobileBackendApplication.java
@@ -0,0 +1,13 @@
+package io.webMobile.webMobileBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMobileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMobileBackendApplication.class, args);
+	}
+
+}
---
+++ webMobileBackend/webMobileBackend/src/test/java/io/webMobile/webMobileBackend/WebMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.webMobile.webMobileBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMobileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
